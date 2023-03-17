13a3d9cac81b95d8dbedcd7d554549250134c4d4
Pedro Monteiro <pedromont436@gmail.com>
2023-03-17 18:20:44 -0300

    first commit

---
+++ src/main/java/com/pedro/logistic/logisticapi/LogisticApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.logistic.logisticapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/logistic/logisticapi/LogisticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.logistic.logisticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
